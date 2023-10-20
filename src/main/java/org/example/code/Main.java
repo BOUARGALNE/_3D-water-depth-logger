c81acbe6058435c86c6f7be56f4ce7aff5eed00c
bouargalne <bouargalne.hamid@gmail.com>
2023-10-20 11:48:23 +0100

    final

--- src/main/java/org/example/code/Main.java
+++ src/main/java/org/example/code/Main.java
@@ -1,13 +1,12 @@
 package org.example.code;
 
-import org.example.code.LinearTriInterpolator;
-import org.locationtech.jts.geom.*;
-import org.locationtech.jts.triangulate.DelaunayTriangulationBuilder;
-import org.apache.commons.math3.analysis.function.Exp;
+import org.apache.commons.math3.analysis.interpolation.BicubicInterpolatingFunction;
+import org.apache.commons.math3.analysis.interpolation.BicubicInterpolator;
+
 import org.apache.commons.math3.analysis.interpolation.LinearInterpolator;
 import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
-
-
+import org.locationtech.jts.geom.*;
+import org.locationtech.jts.triangulate.DelaunayTriangulationBuilder;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -15,8 +14,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class TriangulationExample {
-
+public class Main {
+    private static final GeometryFactory geometryFactory = new GeometryFactory();
     public static void main(String[] args) {
         ArrayList<Double> x = new ArrayList<>();
         ArrayList<Double> y = new ArrayList<>();
@@ -66,21 +65,30 @@ public class TriangulationExample {
         double[] xi = linspace(latmin, latmax, ngridx);
         double[] yi = linspace(lngmin, lngmax, ngridy);
 
-        double[][][] Xi = createMeshGrid(xi, yi);
-/*        // Convertir les coordonnées en objets Coordinate pour la triangulation
+        double[][][] Mi = createMeshGrid(xi, yi);
+        double[][]Xi=new double[Mi[0].length][Mi[0][0].length];
+        double[][]Yi=new double[Mi[0].length][Mi[1][0].length];
+        for (int i = 0; i < Mi[0].length; i++) {
+            for (int j = 0; j < Mi[0][0].length; j++) {
+                Xi[i][j]=Mi[0][i][j];
+                Yi[i][j]=Mi[1][i][j];
+            }
+
+        }
+      // Convertir les coordonnées en objets Coordinate pour la triangulation
         Coordinate[] coordinates = new Coordinate[x.size()];
         for (int i = 0; i < x.size(); i++) {
             coordinates[i] = new Coordinate(x.get(i), y.get(i));
         }
 
         // Créer un objet MultiPoint pour la triangulation
-        GeometryFactory geometryFactory = new GeometryFactory();
-        MultiPoint multiPoint = geometryFactory.createMultiPointFromCoords(coordinates);
+        GeometryFactory geometryFactory1 = new GeometryFactory();
+        MultiPoint multiPoint = geometryFactory1.createMultiPointFromCoords(coordinates);
 
         // Effectuer la triangulation de Delaunay
         DelaunayTriangulationBuilder triangulator = new DelaunayTriangulationBuilder();
         triangulator.setSites(multiPoint);
-        Polygon[] triangles = getTriangles(triangulator.getTriangles(geometryFactory));
+        Polygon[] triangles = getTriangles(triangulator.getTriangles(geometryFactory1));
       for (Polygon triangle : triangles) {
             // Faites quelque chose avec le triangle, par exemple, accédez aux sommets du triangle
             Coordinate[] triangleCoordinates = triangle.getCoordinates();
@@ -89,22 +97,65 @@ public class TriangulationExample {
                 double yVertex = vertex.getY();
 
             }
-        }*/
-
- /*       LinearTriInterpolator interpolator = new LinearTriInterpolator(xi, yi, z.stream().mapToDouble(Double::doubleValue).toArray());
-        double[][] zi = new double[ngridx][ngridy];
-
-        for (int i = 0; i < ngridx; i++) {
-            for (int j = 0; j < ngridy; j++) {
-                zi[i][j] = interpolator.interpolate(xi[i], yi[j]);
-            }
-        }*/
-        double[] x1=listToArray(x);
-        double[] y1=listToArray(x);
-        double[] z1=listToArray(x);
-        double[][] zi = interpolateValues(xi, yi, x1, y1, z1);
-
+        }
+        double[]Z=listToArray(z);
+        double[][] zi = linearInterpolate(triangles,Z, Xi, Yi);
     }
+
+public static double[][] linearInterpolate(Polygon[] triangles, double[] z, double[][] Xi, double[][] Yi) {
+    int n = Xi.length;
+    int m = Xi[0].length;
+    double[][] zi = new double[n][m];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            double interpolatedX = Xi[i][j];
+            double interpolatedY = Yi[i][j];
+
+            // Trouver le triangle contenant les coordonnées interpolées
+            for (int k = 0; k < triangles.length; k++) {
+                Polygon triangle = triangles[k];
+                if (triangle.contains(geometryFactory.createPoint(new Coordinate(interpolatedX, interpolatedY)))) {
+                    // Calculer la valeur interpolée zi[i][j] à partir des coordonnées interpolées et du triangle
+                    zi[i][j] = interpolateInsideTriangle(interpolatedX, interpolatedY, triangle, z, k);
+                    break;
+                }
+            }
+        }
+    }
+
+    return zi;
+}
+    public static double interpolateInsideTriangle(double x, double y, Polygon triangle, double[] z, int triangleIndex) {
+        Coordinate[] vertices = triangle.getCoordinates();
+        Coordinate v0 = vertices[0];
+        Coordinate v1 = vertices[1];
+        Coordinate v2 = vertices[2];
+
+        // Calculer les coordonnées barycentriques du point (x, y) par rapport au triangle
+        double denominator = ((v1.y - v2.y) * (v0.x - v2.x) + (v2.x - v1.x) * (v0.y - v2.y));
+        double alpha = ((v1.y - v2.y) * (x - v2.x) + (v2.x - v1.x) * (y - v2.y)) / denominator;
+        double beta = ((v2.y - v0.y) * (x - v2.x) + (v0.x - v2.x) * (y - v2.y)) / denominator;
+        double gamma = 1.0 - alpha - beta;
+
+        // Vérifier les indices avant d'accéder au tableau z
+        int index0 = triangleIndex * 3;
+        int index1 = index0 + 1;
+        int index2 = index0 + 2;
+
+        // Assurer que les indices sont valides
+        if (index0 >= 0 && index0 < z.length && index1 >= 0 && index1 < z.length && index2 >= 0 && index2 < z.length) {
+            // Utiliser les coordonnées barycentriques pour interpoler la valeur z à l'intérieur du triangle
+            double interpolatedZ = alpha * z[index0] + beta * z[index1] + gamma * z[index2];
+            return interpolatedZ;
+        } else {
+            // Si les indices ne sont pas valides, renvoyer une valeur par défaut ou gérer l'erreur selon votre besoin
+            return 0.0; // Valeur par défaut (ajustez selon votre logique)
+        }
+    }
+
+
+
     public static double[] listToArray(ArrayList<Double> list) {
         double[] array = new double[list.size()];
         for (int i = 0; i < list.size(); i++) {
@@ -112,7 +163,7 @@ public class TriangulationExample {
         }
         return array;
     }
-/*    private static Polygon[] getTriangles(org.locationtech.jts.geom.Geometry trianglesGeometry) {
+  private static Polygon[] getTriangles(org.locationtech.jts.geom.Geometry trianglesGeometry) {
         List<Polygon> triangles = new ArrayList<>();
         for (int i = 0; i < trianglesGeometry.getNumGeometries(); i++) {
             if (trianglesGeometry.getGeometryN(i) instanceof Polygon) {
@@ -120,27 +171,6 @@ public class TriangulationExample {
             }
         }
         return triangles.toArray(new Polygon[0]);
-    }*/
-    private static double[][] interpolateValues(double[] xi, double[] yi, double[] x, double[] y, double[] z) {
-        double[][] zi = new double[xi.length][yi.length];
-
-        LinearInterpolator interpolator = new LinearInterpolator();
-        for (int i = 0; i < xi.length; i++) {
-            for (int j = 0; j < yi.length; j++) {
-                // Interpolation 1D pour obtenir les valeurs zi
-                PolynomialSplineFunction splineFunctionX = interpolator.interpolate(x, z);
-                PolynomialSplineFunction splineFunctionY = interpolator.interpolate(y, z);
-
-                // Interpolation 2D en utilisant les fonctions 1D
-                double interpolatedValueX = splineFunctionX.value(xi[i]);
-                double interpolatedValueY = splineFunctionY.value(yi[j]);
-
-                // Calculer la valeur interpolée en (xi, yi)
-                zi[i][j] = interpolatedValueX + interpolatedValueY;
-            }
-        }
-
-        return zi;
     }
     public static double[][][] createMeshGrid(double[] xi, double[] yi) {
         double[][][] grids = new double[2][yi.length][xi.length];
